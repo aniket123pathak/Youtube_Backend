32c12d49546497ffa290073c6021670a2c8f785d
Aniket Pathak <aniketpathak581@gmail.com>
2025-08-24 14:55:17 +0530

    revert

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -31,23 +31,18 @@ const registerUser = asyncHandler( async (req, res) => {
     if (existedUser) {
         throw new ApiError(409, "User with email or username already exists")
     }
+    
     console.log(req.files)
 
     const avatarLocalPath = req.files?.avatar[0]?.path;
-    //const coverImageLocalPath = req.files?.coverImage[0]?.path;
-
-    let coverImageLocalPath;
-    if (req.files && Array.isArray(req.files.coverImage) && req.files.coverImage.length > 0) {
-        coverImageLocalPath = req.files.coverImage[0].path
-    }
-    
+    const coverimageLocalPath = req.files?.coverimage[0]?.path;
 
     if (!avatarLocalPath) {
         throw new ApiError(400, "Avatar file is required")
     }
 
     const avatar = await uploadOnCloudinary(avatarLocalPath)
-    const coverImage = await uploadOnCloudinary(coverImageLocalPath)
+    const coverimage = await uploadOnCloudinary(coverimageLocalPath)
 
     if (!avatar) {
         throw new ApiError(400, "Avatar file is required")
@@ -57,7 +52,7 @@ const registerUser = asyncHandler( async (req, res) => {
     const user = await User.create({
         fullName,
         avatar: avatar.url,
-        coverImage: coverImage?.url || "",
+        coverimage: coverimage?.url || "",
         email, 
         password,
         username: username.toLowerCase()
